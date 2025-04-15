a87b7f1d2f3f904de5b4d98f96884f297d1dcc01
Juan Camilo Cuspián <cuspianhuertas@gmail.com>
2025-04-15 10:12:13 -0500

    fred

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/codeloom/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
